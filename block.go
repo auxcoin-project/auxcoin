9fbcf58817f0fc188fd6d54365105c7a6f04f5f4
Auxcoin Developer <auxcoin@protonmail.com>
2018-02-07 01:56:47 -0500

    add block

---
+++ block.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+
+	"github.com/pkg/errors"
+)
+
+type (
+	Block struct {
+		Timestamp int64
+		Bits      uint32
+		Nonce     uint32
+		Hash      []byte
+		PrevHash  []byte
+		Data      []byte
+	}
+)
+
+func NewBlock(tstamp int64, bits uint32, data []byte) *Block {
+	return &Block{
+		Timestamp: tstamp,
+		Bits:      bits,
+		//Nonce:   0,
+		//Hash:    []byte{},
+		//PrevHash: []byte{},
+		Data: data,
+	}
+}
+
+func (b Block) Encode() ([]byte, error) {
+	var result bytes.Buffer
+	if err := gob.NewEncoder(&result).Encode(b); err != nil {
+		return []byte{}, errors.Wrap(err, "failed to encode block")
+	}
+	return result.Bytes(), nil
+}
+
+func DecodeBlock(encoded []byte) (*Block, error) {
+	var b Block
+	buf := bytes.NewBuffer(encoded)
+	if err := gob.NewDecoder(buf).Decode(&b); err != nil {
+		return nil, errors.Wrap(err, "failed to decode block")
+	}
+	return &b, nil
+}
---
+++ block_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBlock(t *testing.T) {
+	t.Parallel()
+
+	t.Run("new block", func(t *testing.T) {
+		expected := &Block{
+			Timestamp: time.Now().Unix(),
+			Bits:      42,
+			Data:      []byte("data"),
+		}
+		actual := NewBlock(expected.Timestamp, expected.Bits, expected.Data)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("encoding", func(t *testing.T) {
+		block := NewBlock(time.Now().Unix(), 42, []byte("data"))
+
+		encoded, err := block.Encode()
+		assert.NoError(t, err)
+		assert.NotEmpty(t, encoded)
+
+		decoded, err := DecodeBlock(encoded)
+		assert.NoError(t, err)
+		assert.Equal(t, block, decoded)
+	})
+}
